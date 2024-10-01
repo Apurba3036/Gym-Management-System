60eaee782ca84bfdb529ae77fb429f2554b25e79
apurba <nazmussakibapurbo@gmail.com>
2024-10-01 12:24:58 -0700

    gym project

---
+++ frontend/src/config/firebase.init.js
@@ -0,0 +1,39 @@
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZfbxqsit7um5co-FHuvWKX6aO5ARMb-I",
+  authDomain: "music-cons.firebaseapp.com",
+  projectId: "music-cons",
+  storageBucket: "music-cons.appspot.com",
+  messagingSenderId: "224145904923",
+  appId: "1:224145904923:web:af37627e05613beccf2fbe"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+*/
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbsU6n5Mz36N1VUUbkDAwTHaSWy6vnzeY",
+  authDomain: "workout-master-cee3b.firebaseapp.com",
+  projectId: "workout-master-cee3b",
+  storageBucket: "workout-master-cee3b.appspot.com",
+  messagingSenderId: "33335769129",
+  appId: "1:33335769129:web:8a7e26c8341e99965bc438",
+  measurementId: "G-XTZRQXB7TG"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
